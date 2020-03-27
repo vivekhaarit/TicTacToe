9edb236400f735219a1aadd5bf5a7d4e75377f24
vivek kumar <vivekhaarit@gmail.com>
2020-03-27 10:48:12 +0530

    [vivek]Added:user can see its inputs

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,20 +1,47 @@
-#!/bin/bash
+#!/bin/bash 
  
 echo "----WELCOME TO TIC TAC TOE------"
 echo "  you will play against the CPU"
 
 #play board creation
-
-val=(" " " " " " " " " " " " " " " " " ")
+ar=(" " " " " " " " " " " " " " " " " ")
 echo "----------------------------------"
 echo "user ip: x               cpu ip: 0"
 echo "----------------------------------"
 echo  "press T to do a toss"
 read toss_choice
 
+ShowTheBoard(){
+	echo " ${ar[0]} | ${ar[1]} | ${ar[2]} "
+	echo "---|---|---"
+	echo " ${ar[3]} | ${ar[4]} | ${ar[5]} "
+	echo "---|---|---"
+	echo " ${ar[6]} | ${ar[7]} | ${ar[8]} "
+}
+
 Terminate(){
 	echo "---------------YOU'R OUT OF THE GAME--------------"
 	exit 
+}
+
+PlayCPU(){
+	echo "cpu played"
+	ShowTheBoard
+	PlayUser
+}
+
+####
+
+PlayUser(){
+	echo "enter row(1-3), col(1-3):"
+	read -p "Row No:" r
+	read -p "Col No:" c
+	i=$(($(( (($(($r-1)) ))*3)) + $(($c-1)) ))
+	ar[$i]="x"
+
+	ShowTheBoard
+	#CheckUserWinOrLose
+	PlayCPU
 }
 
 TossChoice (){
@@ -24,7 +51,7 @@ TossChoice (){
 		then 
 			toss_result="T"
 			echo "You won the toss. Start playing.."
-			# PlayUser
+			PlayUser
 		else
 			toss_result="H"
 			echo "CPU won the toss."
@@ -44,16 +71,3 @@ TossChoice (){
 }
 
 TossChoice
-
-echo " ${val[0]} | ${val[1]} | ${val[2]} "
-echo "---|---|---"
-echo " ${val[3]} | ${val[4]} | ${val[5]} "
-echo "---|---|---"
-echo " ${val[6]} | ${val[7]} | ${val[8]} "
-
-
-# ResetBoard(){
-# 	case $1 in 
-# 		"r")
-		
-# }
