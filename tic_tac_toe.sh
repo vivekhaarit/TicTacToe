e67c4879beacbba70bf4b54c24ba531b9dd54eab
vivek kumar <vivekhaarit@gmail.com>
2020-03-27 07:19:26 +0530

    [vivek]Added:toss feature

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -6,9 +6,45 @@ echo "  you will play against the CPU"
 #play board creation
 
 val=(" " " " " " " " " " " " " " " " " ")
-echo "user ip: x"
-echo "cpu ip: 0"
-echo "----------------"
+echo "----------------------------------"
+echo "user ip: x               cpu ip: 0"
+echo "----------------------------------"
+echo  "press T to do a toss"
+read toss_choice
+
+Terminate(){
+	echo "---------------YOU'R OUT OF THE GAME--------------"
+	exit 
+}
+
+TossChoice (){
+	if [ $toss_choice == "T" ] || [ $toss_choice == "t" ]
+	then
+		if [ $(($RANDOM%2)) -eq 0 ]
+		then 
+			toss_result="T"
+			echo "You won the toss. Start playing.."
+			# PlayUser
+		else
+			toss_result="H"
+			echo "CPU won the toss."
+			# PlayCPU
+		fi
+	else
+		read -p "INVALID INPUT: press Enter to try again, E to exit>> " tryexit
+		if [[ -z "$tryexit" ]]
+		then
+			TossChoice
+		elif [ "$tryexit" == "e" ]
+		then
+			Terminate
+		fi 
+	
+	fi
+}
+
+TossChoice
+
 echo " ${val[0]} | ${val[1]} | ${val[2]} "
 echo "---|---|---"
 echo " ${val[3]} | ${val[4]} | ${val[5]} "
