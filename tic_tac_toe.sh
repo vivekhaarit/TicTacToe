74074cf7c228e95bf815bac2aa96213030a13c55
vivek kumar <vivekhaarit@gmail.com>
2020-03-27 06:56:00 +0530

    [vivek]Added:start with empty board

--- tic_tac_toe.sh
+++ tic_tac_toe.sh
@@ -1,60 +1,11 @@
 #!/bin/bash
+ 
+echo "----WELCOME TO TIC TAC TOE------"
+echo "  you will play against the CPU"
+
+#play board creation
 
 val=(" " " " " " " " " " " " " " " " " ")
-echo "user ip: x"
-echo "cpu ip: 0"
-echo "----------------"
-echo  "press T to do a toss"
-read toss_choice
-
-Terminate(){
-	echo "---------------YOU'R OUT OF THE GAME--------------"
-	exit 
-}
-
-TossChoice (){
-	if [ $toss_choice == "T" ] || [ $toss_choice == "t" ]
-	then
-		if [ $(($RANDOM%2)) -eq 0 ]
-		then 
-			toss_result="T"
-		else
-			toss_result="H"
-		fi
-	else
-		read -p "INVALID INPUT: press Enter to try again, E to exit>> " tryexit
-		if [[ -z "$tryexit" ]]
-		then
-			TossChoice
-		elif [ $tryexit == "e" ]
-		then
-			Terminate
-		fi 
-	
-	fi
-}
-
-TossChoice
-
-#USER PLAYING
-if [ "$toss_result" == "T" ]
-then
-	echo "You won the Toss. Start playing."
-	echo "enter row(1-3), col(1-3):"
-	read -p "Row No:" r
-	read -p "Col No:" c
-	i=$(($(( (($(($r-1)) ))*3)) + $(($c-1)) ))
-	val[$i]="x"
-fi
-
-#COMPUTER PLAYING
-if [ "$toss_result" == "H" ]
-then
-	echo "CPU won the toss."
-	echo "computer's turn"
-fi
-
-echo ""
 echo " ${val[0]} | ${val[1]} | ${val[2]} "
 echo "---|---|---"
 echo " ${val[3]} | ${val[4]} | ${val[5]} "
@@ -62,10 +13,8 @@ echo "---|---|---"
 echo " ${val[6]} | ${val[7]} | ${val[8]} "
 
 
-
-
-
-
-
-
-
+# ResetBoard(){
+# 	case $1 in 
+# 		"r")
+		
+# }
